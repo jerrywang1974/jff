946f0793c6bd7d8113591e036f3dfeb3c936594e
Yubao Liu <yubao.liu@gmail.com>
2015-10-15 23:08:24 +0800

    a script to synchronize between git remotes

---
+++ utils/sync-from-git-upstream-remote-my-remote.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+upstream="$1"
+my="$2"
+
+[ "$upstream" ] && [ "$my" ] || {
+    echo "Usage: $0 UPSTREAM-REMOTE MY-REMOTE" >&2
+    exit 1
+}
+
+echo "Run 'git remote update'..."
+git remote update
+
+git branch -a | fgrep remotes/$upstream/ |
+    fgrep -v remotes/$upstream/HEAD |
+    while read branch; do
+        b=${branch#remotes/$upstream/}
+        [ "$b" ] && {
+            echo "Sync $branch to remotes/$my/$b..."
+            git push $my $branch:$b
+        }
+    done
+
+echo "Push tags..."
+git push --tags $my
+
