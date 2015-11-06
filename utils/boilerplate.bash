a68b7f45097b63d95f5e8252a0cfadf87c84c927
Yubao Liu <yubao.liu@gmail.com>
2015-11-06 16:05:02 +0800

    save

---
+++ utils/boilerplate.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+# use ${1:?} to check whether $1 is unset or empty
+# use ${1?} to check whether $1 is unset
+
