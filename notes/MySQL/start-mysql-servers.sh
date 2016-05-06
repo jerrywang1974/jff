30440a19e93d8d7881acbb7946e43a3aa1e946a4
Yubao Liu <yubao.liu@gmail.com>
2016-05-06 16:14:19 +0800

    enable log-slave-updates

--- notes/MySQL/start-mysql-servers.sh
+++ notes/MySQL/start-mysql-servers.sh
@@ -3,14 +3,14 @@
 wait_for_mysql_servers_started() {
     for i in 1 2 3; do
         while :; do
-            docker logs --tail=3 mysql_$i | fgrep -q 'ready for connections'&& break
+            docker exec -it mysql_$i bash -c "mysql -h mysql_$i </dev/null 2>/dev/null" && break
             sleep 1
         done
     done
 }
 
 
-docker network create mysql-net
+docker network create mysql-net 2>/dev/null
 
 for i in 1 2 3; do
     docker run -dt --net=mysql-net --name mysql_$i --hostname mysql_$i -e MYSQL_ALLOW_EMPTY_PASSWORD=true mysql
@@ -31,19 +31,19 @@ wait_for_mysql_servers_started
 
 docker exec -i mysql_1 bash <<'EOF'
 for i in 2 3; do
-    mysql -h mysql_1 -e "CHANGE MASTER TO MASTER_HOST='mysql_$i', MASTER_PORT=3306, MASTER_USER='root', MASTER_PASSWORD='', MASTER_AUTO_POSITION=1 FOR CHANNEL 'mysql_$i'"
+    mysql -h mysql_1 -e "CHANGE MASTER TO MASTER_HOST='mysql_$i', MASTER_PORT=3306, MASTER_AUTO_POSITION=1 FOR CHANNEL 'mysql_$i'"
 done
 
 for i in 1 3; do
-    mysql -h mysql_2 -e "CHANGE MASTER TO MASTER_HOST='mysql_$i', MASTER_PORT=3306, MASTER_USER='root', MASTER_PASSWORD='', MASTER_AUTO_POSITION=1 FOR CHANNEL 'mysql_$i'"
+    mysql -h mysql_2 -e "CHANGE MASTER TO MASTER_HOST='mysql_$i', MASTER_PORT=3306, MASTER_AUTO_POSITION=1 FOR CHANNEL 'mysql_$i'"
 done
 
 for i in 1 2; do
-    mysql -h mysql_3 -e "CHANGE MASTER TO MASTER_HOST='mysql_$i', MASTER_PORT=3306, MASTER_USER='root', MASTER_PASSWORD='', MASTER_AUTO_POSITION=1 FOR CHANNEL 'mysql_$i'"
+    mysql -h mysql_3 -e "CHANGE MASTER TO MASTER_HOST='mysql_$i', MASTER_PORT=3306, MASTER_AUTO_POSITION=1 FOR CHANNEL 'mysql_$i'"
 done
 
 for i in 1 2 3; do
-    mysql -h mysql_$i -e "start slave"
+    mysql -h mysql_$i -e "start slave USER='root' PASSWORD=''"
 done
 EOF
 
