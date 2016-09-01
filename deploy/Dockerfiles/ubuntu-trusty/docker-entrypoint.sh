b9d15905538481f13d0b106d6d755670e655ff3e
Yubao Liu <yubao.liu@gmail.com>
2016-09-01 19:13:50 +0800

    base docker image for deploy.mk

---
+++ deploy/Dockerfiles/ubuntu-trusty/docker-entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+USE_TINI=${USE_TINI,,}
+USE_DUMB_INIT=${USE_DUMB_INIT,,}
+if [ $$ = 1 -a ${USE_TINI:-true} = true ]; then
+    export USE_TINI=used
+    [ -x /usr/local/bin/tini ] && exec /usr/local/bin/tini $0 -- "$@"
+fi
+if [ $$ = 1 -a ${USE_DUMB_INIT:-true} = true ]; then
+    export USE_DUMB_INIT=used
+    [ -x /bin/dumb-init ] && exec /bin/dumb-init $0 "$@"
+fi
+unset USE_TINI
+unset USE_DUMB_INIT
+
+
+: ${DOCKER_HOST_IP_FILE:=/etc/host-ip}
+if [ -s "$DOCKER_HOST_IP_FILE" ]; then
+    read -r host_ip < "$DOCKER_HOST_IP_FILE"
+    if [ "$host_ip" ]; then
+        export CONSUL_SERVICE_HOST="$host_ip"
+        export CONSUL_SERVICE_PORT="${CONSUL_SERVICE_PORT:-8500}"
+
+        if [ -w /etc/hosts ]; then
+            printf "\n%s\t%s\n" $host_ip consul >> /etc/hosts
+        fi
+    fi
+fi
+unset DOCKER_HOST_IP_FILE
+
+exec "$@"
+
