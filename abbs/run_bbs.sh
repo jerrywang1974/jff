a9537e909399546ba3c5d03c47c09f54ade43709
Yubao Liu <yubao.liu@gmail.com>
2016-10-06 17:12:57 +0800

    delete some old stuffs

--- abbs/run_bbs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -x
-
-./bbs 100 for_test/myboard for_test/pool `pwd`/for_test/tmp
-
--- abbs/run_boardd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -x
-
-mkdir -p for_test/pool for_test/tmp 2>/dev/null
-
-./boardd for_test/pool 100 for_test/myboard
-
