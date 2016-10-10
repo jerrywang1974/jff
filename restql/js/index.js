e40a878ce1216efc63cde7b6213db5b7c4700f31
Yubao Liu <yubao.liu@gmail.com>
2016-10-10 12:10:25 +0800

    JS demo

---
+++ restql/js/index.js
@@ -0,0 +1,39 @@
+const Sequelize = require('sequelize');
+const cache = require('apicache').middleware;
+const compression = require('compression');
+const express = require('express');
+const _ = require('lodash');
+
+const db = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    pool: {
+        max: 50,
+        min: 0,
+        idle: 30000,
+    }
+});
+
+const queries = {
+    'order': {
+        'sql': 'SELECT * FROM order LIMIT :n',
+        'db': db
+    }
+};
+
+const app = express();
+
+app.use(compression());
+
+app.get('/q/:q', cache('30 seconds'), function (req, res) {
+    const query = queries[req.params.q];
+    query['db'].query(query['sql'], {
+        replacements: _.cloneDeepWith(req.query, v => isNaN(v) ? undefined : +v),
+        type: Sequelize.QueryTypes.SELECT
+    }).then(function (rows) {
+        res.json(rows);
+    });
+});
+
+app.listen(process.env.PORT || 8080);
+
