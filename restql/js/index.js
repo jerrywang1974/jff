f20b4444388c1531165d01316adbb3d405b8bc51
Yubao Liu <yubao.liu@gmail.com>
2016-10-10 12:19:22 +0800

    Python demo and use @ to escape numbers
    The Python version is much worse, doesn't support compression and cache,
    module records is buggy:
    * doesn't support "IN (:pay_status)"
    * "IN :pay_status" doesn't support single "pay_status" parameter in HTTP request
    * rows.export('json') raises IndexError if rows is empty
    * rows.export('json') can't serialize Decimal objects if no third party
      JSON module is installed.

--- restql/js/index.js
+++ restql/js/index.js
@@ -16,7 +16,7 @@ const db = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PAS
 
 const queries = {
     'order': {
-        'sql': 'SELECT * FROM order LIMIT :n',
+        'sql': 'SELECT * FROM order WHERE pay_status IN (:pay_status) LIMIT :n',
         'db': db
     }
 };
@@ -28,7 +28,10 @@ app.use(compression());
 app.get('/q/:q', cache('30 seconds'), function (req, res) {
     const query = queries[req.params.q];
     query['db'].query(query['sql'], {
-        replacements: _.cloneDeepWith(req.query, v => isNaN(v) ? undefined : +v),
+        replacements: _.cloneDeepWith(req.query, v =>
+                              typeof(v) != 'string' ? undefined :
+                              v.startsWith('@@') ? v.substring(1) :
+                              v.startsWith('@') ? +v.substring(1) : v),
         type: Sequelize.QueryTypes.SELECT
     }).then(function (rows) {
         res.json(rows);
---
+++ restql/python/main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+from bottle import get, request, response, run
+import os
+import records
+
+db = records.Database(os.getenv('DB_URL'))
+
+queries = {
+    'order': {
+        'sql': 'SELECT * FROM order WHERE pay_status IN :pay_status LIMIT :n',
+        'db': db
+    }
+}
+
+def digitize(s):
+    if s.startswith('@@'):
+        return s[1:]
+    if s.startswith('@'):
+        s = s[1:]
+        try: return int(s)
+        except: return float(s)
+    else:
+        return s
+
+def normalize(v):
+    if len(v) == 1: return digitize(v[0])
+    else: return tuple(map(digitize, v))
+
+@get('/q/<q>')
+def select(q):
+    query = queries[q]
+    params = request.query
+    params = { k: normalize(params.getall(k)) for k in params.keys() }
+    rows = query['db'].query(query['sql'], **params)
+    response.content_type = 'application/json; charset=utf-8'
+    return rows.export('json')
+
+run(host='0.0.0.0', port=8080)
+
