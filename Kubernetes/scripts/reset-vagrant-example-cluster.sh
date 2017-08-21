949227925b6dd7eb56b9e01833b5afd01310e5ee
Yubao Liu <yubao.liu@gmail.com>
2017-08-22 00:24:41 +0800

    save

--- Kubernetes/scripts/reset-vagrant-example-cluster.sh
+++ Kubernetes/scripts/reset-vagrant-example-cluster.sh
@@ -3,8 +3,8 @@
 # https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/#tear-down
 
 for i in 4 3 2 1; do
-    ssh node01 "kubectl drain node0$i --delete-local-data --force --ignore-daemonsets"
-    ssh node01 "kubectl delete node node0$i"
+    ssh node01 "sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf drain node0$i --delete-local-data --force --ignore-daemonsets"
+    ssh node01 "sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf delete node node0$i"
     ssh node0$i 'sudo kubeadm reset'
     ssh node0$i 'sudo docker ps -a | grep -v CON | awk "{print \$1}" | sudo xargs -n 1 docker rm -f'
     ssh node0$i 'sudo rm -rf /data0/var/lib/etcd'
