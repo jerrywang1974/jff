91e05ec052ec955b1d40570597cd9d09f92f09b1
Yubao Liu <yubao.liu@gmail.com>
2017-11-13 12:41:00 +0800

    more checks

--- Kubernetes/scripts/setup-kubernetes-high-availability.sh
+++ Kubernetes/scripts/setup-kubernetes-high-availability.sh
@@ -8,7 +8,8 @@ STANDBY="$2"
 API_SERVER="$3"
 
 API_SERVER=${API_SERVER%/}
-[ "${API_SERVER#https://}" != "$API_SERVER" ] || {
+api_host=${API_SERVER#https://}
+[ "$api_host" != "$API_SERVER" -a "${api_host%:*}" != "$api_host" ] || {
     echo "Usage: $0 MASTER-HOST STANDBY-HOST https://your-k8s-api.example.com:6443" >&2
     exit 1
 }
@@ -17,8 +18,16 @@ echo "Nodes in the cluster:"
 kubectl get nodes --show-labels
 echo
 
+[ "$YES" = "y" ] || read -p "Continue? [yN]" YES
+[ "$YES" = "y" ] || exit 0
+
 [ "`ssh root@$MASTER sha256sum /etc/kubernetes/pki/ca.crt`" = "`ssh root@$STANDBY sha256sum /etc/kubernetes/pki/ca.crt`" ] || {
     echo "ERROR: /etc/kubernetes/pki/ca.crt not same on $MASTER and $STANDBY, they don't belong to same K8S cluster!" >&2
+    exit 1
+}
+
+ssh root@$STANDBY "echo | openssl s_client -connect ${API_SERVER#https://} 2>/dev/null | openssl x509 -noout -text | fgrep -q 'DNS:$STANDBY'" || {
+    echo "ERROR: Certificate in $API_SERVER doesn't contain DNS:$STANDBY in Subject Alternative Name" >&2
     exit 1
 }
 
@@ -58,7 +67,7 @@ advertise_ip=`ssh root@$STANDBY "ip -4 -o route get $api_ip | sed -e 's/^.*src /
 
 for f in /etc/kubernetes/manifests/kube-{apiserver,controller-manager,scheduler}.yaml; do
     echo "copy $f [advertise_ip=$advertise_ip] ..."
-    ssh root@$MASTER cat $f | perl -pe "s|--advertise-address=\S+$|--advertise-address=$advertise_ip|" | ssh root@$STANDBY "umask 0077; cat > $f"
+    ssh root@$MASTER cat $f | perl -pe "s|--advertise-address=\S+$|--advertise-address=$advertise_ip|" | ssh root@$STANDBY "umask 0077 && mkdir -p /etc/kubernetes/manifests && cat > $f"
 done
 
 
@@ -70,6 +79,8 @@ ssh root@$STANDBY '[ ! -x /usr/bin/etckeeper ] || ! etckeeper unclean || etckeep
 
 echo "update configmap kube-proxy ..."
 kubectl get configmap kube-proxy -n kube-system -o yaml | perl -pe "s|\bserver:.*$|server: $API_SERVER|" | kubectl apply -f -
-echo "You may need to run 'kubectl delete pod -n kube-system -l k8s-app=kube-proxy'."
-echo "You may need to update \"server\" in /etc/kubernetes/kubelet.conf on all your worker nodes."
+
+echo
+echo "!!! You may need to run 'kubectl delete pod -n kube-system -l k8s-app=kube-proxy'."
+echo "!!! You may need to update \"server\" in /etc/kubernetes/kubelet.conf on all your worker nodes."
 
