62e5d26976c70dc38402aeb7449d80c72b577e48
Yubao Liu <yubao.liu@gmail.com>
2018-03-01 00:06:27 +0800

    remove old files

--- Lisp/MyLisp/docs/ctx.bat
+++
@@ -1,4 +0,0 @@
-set path=c:\ruby\bin;%path%
-set OSFONTDIR=E:\jff\tex\ConTeXt-MkIV\Adobe-Fonts;C:\Windows\Fonts
-E:\dev\context\tex\setuptex.bat E:\dev\context\tex
-
--- Lisp/MyLisp/setenv.bat
+++
@@ -1,2 +0,0 @@
-set path=C:\MinGW\bin;d:\program\LLVM\llvm-2.4;d:\program\LLVM\llvm-gcc-4.2\bin;%path%
-
