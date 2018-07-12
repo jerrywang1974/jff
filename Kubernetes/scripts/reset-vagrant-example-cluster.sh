ce7d6ddabf84b90bb9993f917f13687d186e57c4
Yubao Liu <yubao.liu@gmail.com>
2018-07-12 19:19:54 +0800

    k8s v1.11

--- Kubernetes/scripts/reset-vagrant-example-cluster.sh
+++ Kubernetes/scripts/reset-vagrant-example-cluster.sh
@@ -5,8 +5,8 @@
 for i in 3 2 1; do
     ssh node01 "sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf drain node0$i --delete-local-data --force --ignore-daemonsets"
     ssh node01 "sudo kubectl --kubeconfig=/etc/kubernetes/admin.conf delete node node0$i"
-    ssh node0$i 'sudo kubeadm reset'
-    ssh node0$i 'sudo docker ps -a | grep -v CON | awk "{print \$1}" | sudo xargs -n 1 docker rm -f'
-    ssh node0$i 'sudo rm -rf /data/var/lib/etcd'
+    ssh node0$i 'sudo kubeadm reset -f'
+    ssh node0$i 'sudo docker ps -a | grep -v CON | awk "{print \$1}" | sudo xargs -n 1 --no-run-if-empty docker rm -f'
+    ssh node0$i 'sudo rm -rf /data/var/lib/etcd /var/lib/kube* /etc/systemd/system/kubelet.service.d/60-kubeadm-override.conf'
 done
 
